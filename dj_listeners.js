489ca72fe8b27acf071574c27bdf63604d4093e4
Walter Menendez <walterm@mit.edu>
2015-04-14 16:28:35 -0400

    Listener logic for the gestures

---
+++ dj_listeners.js
@@ -0,0 +1,50 @@
+function volumeLevel (difference) {
+    return Math.max(Math.min( (1/500.0) * difference + 0.5, 1), 0);
+}
+
+function swipeListener (gesture) {
+    var duration = gesture.duration,
+        direction = gesture.direction[2] < 0 ? 1: -1,
+        isHorizontal, difference;
+
+
+    difference = direction * Math.abs(gesture.position[2] - gesture.startPosition[2]);
+
+    isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[2]);
+
+    if (!isHorizontal) {
+        var newVolume = volumeLevel(difference);
+        sound.play(function (id) {
+            sound.volume(newVolume);
+        });
+    } else {
+
+    }
+}
+
+// TODO: automate this process
+function getSongLength (song) {
+    var songMap = {
+        "badKids": 130000,
+        "titanium": 245000000,
+        "queen": 312000000,
+        "stay": 241000000,
+        "youngAndBeautiful": 256000000
+    };
+
+    return songMap[song];
+}
+
+function circleListener (gesture) {
+    var delta = gesture.duration / getSongLength(song),
+        orientation = gesture.normal[1] > 0 ? -1 : 1;
+
+    delta = delta * orientation;
+     // converting back to seconds
+    delta = delta / 1000000;
+    delta = Math.floor(delta);
+    var start = sound.pos();
+    sound.play(function (id) {
+        sound.pos(start + delta, id);
+    });
+}
