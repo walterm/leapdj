fdb00535873e58d3ff4cc5f9ca5d933167e41694
Walter Menendez <walterm@mit.edu>
2015-04-14 18:02:22 -0400

    Fixed bug where song played over itself when controlling volume

--- dj_listeners.js
+++ dj_listeners.js
@@ -7,18 +7,20 @@ function swipeListener (gesture) {
         direction = gesture.direction[2] < 0 ? 1: -1,
         isHorizontal, difference;
 
-
     difference = direction * Math.abs(gesture.position[2] - gesture.startPosition[2]);
 
     isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[2]);
-
     if (!isHorizontal) {
-        var newVolume = volumeLevel(difference);
+        var newVolume = volumeLevel(difference),
+            pos = sound.pos();
+        sound.pause();
         sound.play(function (id) {
+            console.log("lowering volume", newVolume);
             sound.volume(newVolume);
+            sound.pos(pos, id);
         });
     } else {
-
+        // TODO: crossfade volume for second disc
     }
 }
 
